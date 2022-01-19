5f517f159b5a2ac629e3c31619b3e86c62ef51eb
OscarTalero <talero@gmail.com>
2022-01-18 21:35:54 -0500

    Implementado estilos css y template base

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    "/home/oscar/Documentos/Exercices/Project/webpage/djangoblog/blog/static/css",
+]
